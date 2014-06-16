d29987e8b8a50f2174f91cc8c26cf97dceba93bb
Matt Clark <matt@brahma.local>
2014-06-16 21:20:43 +0100

    Added MANIFEST.in and specified PyPi classifiers

--- setup.py
+++ setup.py
@@ -15,7 +15,15 @@ setup(
     description = "A class for creating a nested dictionaries representation of RDF quads or triples",
     long_description = long_desc,
     license = "MIT",
-    keywords = "rdf json dict",
+    keywords = ['rdf','json', 'dict'],
+    classifiers = ['Programming Language :: Python',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Topic :: Text Processing',
+                   'License :: OSI Approved :: MIT License',
+                   'Intended Audience :: Developers',
+                   'Development Status :: 5 - Production/Stable',
+                   'Operating System :: OS Independent'],
     url = "http://github.com/mattclarkdotnet/rdfdict"
 
 )
